70ec866192f8269611b7a45f249e62267fa95a81
richiengo94 <44595449+richiengo94@users.noreply.github.com>
2025-07-03 11:33:47 -0400

    Added deal single card

--- game.py
+++ game.py
@@ -21,15 +21,23 @@ player_hand = []
 dealer_hand = []
 
 def deal_hand(player_hand: list, dealer_hand: list, shoe: Shoe) -> None:
+    """Deals starting hand in alternating order starting with player"""
 
     for i in range(2):
-        player_hand.append(shoe.shoe.pop())
-        dealer_hand.append(shoe.shoe.pop())
+        deal_card(player_hand, shoe)
+        deal_card(dealer_hand, shoe)
 
-def calculate_hand(hand: list) -> int:
+def deal_card(hand: list, shoe: Shoe) -> None:
+    """Deals a single card"""
 
-    hand_sum = 0
-    n_ace = 0
+    hand.append(shoe.shoe.pop())
+
+def calculate_hand(hand: list) -> tuple[int, bool]:
+    """Calculates value of given hand"""
+
+    hand_sum : int = 0
+    n_ace : int = 0
+    blackjack : bool = False
 
     for i in range(len(hand)):
         if(hand[i][0] == "J" or hand[i][0] == "Q" or hand[i][0] == "K"):
@@ -40,18 +48,26 @@ def calculate_hand(hand: list) -> int:
         else:
             hand_sum += int(hand[i][0])
 
+    # Calculates for aces
     for i in range(n_ace):
         if(hand_sum + 10 <= 21):
             hand_sum += 10
 
-    return hand_sum
+    # Determines if hand is a blackjack
+    if(hand_sum == 21 and len(hand) == 2):
+        blackjack = True
+
+    return hand_sum, blackjack
 
 deal_hand(player_hand, dealer_hand, shoe)
 print(player_hand)
 print(dealer_hand)
-player_hand = [("A", "diamond"), ("A", "spade"), ("5", "heart"), ("8", "diamond"), ("8", "diamond")]
+#player_hand = [("A", "diamond"), ("A", "spade"), ("5", "heart"), ("8", "diamond"), ("8", "diamond")]
 print(calculate_hand(player_hand))
 print(calculate_hand(dealer_hand))
+deal_card(player_hand, shoe)
+print(player_hand)
+print(calculate_hand(player_hand))
 
 while(not game.game_over):
     if(game.get_remaining_credit() <= 0):
