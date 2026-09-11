c118ea9e5b0c37f22f9610c0df2fb22fd697c79f
richiengo94 <44595449+richiengo94@users.noreply.github.com>
2026-09-11 09:49:26 -0400

    Display welcome text
    Display welcome text to display surface

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import blackjack
 import game
 from shoe import Shoe
+import pygame
 
 def main():
+
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+
+    pygame.init()
+
+    display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    display_surface.fill((0, 128, 0))  # Fill the screen with green color
+    pygame.display.set_caption("Blackjack")
+
+    game_font = pygame.font.Font('blackjack/assets/fonts/PixeloidMono.ttf', 32)
+
+    text = game_font.render("Welcome to Blackjack!", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    display_surface.blit(text, text_rect)
+
     #start_credit = float(input("Welcome. Enter starting credit: "))
     start_credit = 100.0
 
@@ -18,6 +35,7 @@ def main():
     while(not new_game.game_over):
         #bet = float(input("Enter bet amount: "))
         bet = 10.0
+        pygame.display.update()
 
         blackjack.deal_hand(player, dealer, new_shoe)
         print("Player hand: ")
@@ -34,6 +52,8 @@ def main():
             player.clear_hand()
             dealer.clear_hand()
             new_shoe.check_shoe()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
